289f7c037e2a93997e2c9aefffda1399b8c660f1
Movin Silva <movinpinsara@gmail.com>
2024-12-27 11:22:29 +0530

    refactor: Refactor IPS agent

--- src/ips-agent/ips_agent.py
+++ src/ips-agent/ips_agent.py
@@ -1,64 +1,27 @@
 from letta import LocalClient, RESTClient, ChatMemory
 from config import EMBEDDING_CONFIG, LLM_CONFIG
 from utils import logger
+from constants import NAME, PERSONA_PROMPT, HUMAN_PROMPT
 
 
 class IPSAgent:
     def __init__(self, client: LocalClient | RESTClient):
-        self.name = "ips-agent"
+        self.name = NAME
         self.client = client
 
     def create(self):
         agentId = self.client.get_agent_id(self.name)
 
         if not agentId:
-            ips_persona_prompt = """
-            I am the IPS agent.
-            My role is to manage the Investment Policy Statement (IPS) for clients.
-            I am responsible for creating, updating, and maintaining details of the IPS.
-            I ensure that the IPS aligns with the client's investment objectives, risk tolerance, and constraints.
-            Following are my tasks.
-            - Extract any information regarding the client's IPS from the given prompt.
-            - Update the client's IPS based on those information.
-            - Return any relevant information to the prompt that is already present in the IPS.
-            - If relevant information is not present, ask for it.
-            Following are the components of an IPS.
-                **1. Personal Information**
-                - Demographics (name, gender, address, age)
-                - Employment status
-                - Family/support situation
-                **2. Investment Knowledge & Portfolio**
-                - Investment knowledge level
-                - Market outlook perspective
-                - Current portfolio value
-                **3. Investment Plan**
-                - Initial investment amount
-                - Subsequent investment schedule
-                - Investment goals
-                - Portfolio performance requirements
-                - Time horizon
-                **4. Financial Situation**
-                - Current financial status
-                - Assets and liabilities
-                - Income stability
-                - Potential financial risks/events
-                **5. Risk Profile**
-                - Loss tolerance timeframe
-                - Value decline tolerance
-                - Market volatility response
-                - Illiquid investment comfort level
-                **6. Investment Preferences**
-                - Preferred investment categories
-            """
 
-        new_agent = self.client.create_agent(
-            name=self.name,
-            embedding_config=EMBEDDING_CONFIG,
-            llm_config=LLM_CONFIG,
-            memory=ChatMemory(
-                human="I'm the client",
-                persona=ips_persona_prompt,
-            ),
-        )
+            new_agent = self.client.create_agent(
+                name=self.name,
+                embedding_config=EMBEDDING_CONFIG,
+                llm_config=LLM_CONFIG,
+                memory=ChatMemory(
+                    human=HUMAN_PROMPT,
+                    persona=PERSONA_PROMPT,
+                ),
+            )
 
         logger.info(f"Created {self.name} with ID: {new_agent.id}")
