58bcc6aaf5ab55d5b3026884e0e71743ae64737c
Movin Silva <movinpinsara@gmail.com>
2024-12-30 14:07:03 +0530

    feat: Implement orchestrator agent creation logic

--- src/orchestrator/constants.py
+++ src/orchestrator/constants.py
@@ -7,5 +7,5 @@ I am the orchestrator agent.
 I am responsible for managing the communication between the agents.
 I am responsible to call the relevant tools and process their responses to generate the final response.
 `call_ips` is a tool that sends a message to the IPS agent.
-I can call this tool before generating a response.
+I can call this tool and consider its output before generating a response.
 """
--- src/orchestrator/orchestrator.py
+++ src/orchestrator/orchestrator.py
@@ -1,5 +1,4 @@
 from letta import LocalClient, RESTClient, ChatMemory
-from ips_agent.constants import NAME as IPS_NAME
 from orchestrator.constants import NAME, PERSONA_PROMPT, HUMAN_PROMPT
 from utils import logger
 from config import EMBEDDING_CONFIG, LLM_CONFIG
@@ -9,31 +8,31 @@ class Orchestrator:
     def __init__(self, client: LocalClient | RESTClient):
         self.client = client
 
-    async def call_ips(self, prompt: str) -> str:
-        """
-        Call the IPS agent to generate a response to the user input.
-
-        Args:
-            prompt (str): User input
-
-        Returns:
-            response (str): IPS agent response
-        """
-
-        # if self.client is None:
-        #     logger.error("Client is not initialized")
-        #     return "Error: Client is not initialized"
-        response = await self.client.send_message(
-            message=prompt, agent_name=IPS_NAME, role="user"
-        )
-
-        logger.info(f"IPS response: {response}")
-
-        return response
-
     def create(self):
 
-        call_ips_tool = self.client.create_tool(self.call_ips)
+        def call_ips(prompt: str) -> str:
+            """
+            Call the IPS agent to generate a response to the user input.
+
+            Args:
+                prompt (str): User input
+
+            Returns:
+                response (str): IPS agent response
+            """
+
+            from letta import create_client
+            from ips_agent.constants import NAME as IPS_NAME
+
+            lClient = create_client()
+
+            response = lClient.send_message(
+                message=prompt, agent_name=IPS_NAME, role="user"
+            )
+
+            return response.messages[len(response.messages) - 2].tool_call.arguments
+
+        call_ips_tool = self.client.create_tool(call_ips)
 
         self.client.create_agent(
             name=NAME,
