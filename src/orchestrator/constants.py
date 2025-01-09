1ca558ade8075dc452bf8fd35d20dc467403e59d
Sandev <sandev.kuruppu@h2o.ai>
2025-01-09 16:13:03 +0530

    feat: add orchestrator feat

--- src/orchestrator/constants.py
+++ src/orchestrator/constants.py
@@ -6,6 +6,10 @@ PERSONA_PROMPT = """
 I am the orchestrator agent.
 I am responsible for managing the communication between the agents.
 I am responsible to call the relevant tools and process their responses to generate the final response.
-`call_ips` is a tool that sends a message to the IPS agent.
+Here are some tools you have
+- `call_ips` is a tool that sends a message to the IPS agent.
+- `call_analysis_agent_tool` is a tool that sens a message to analysis agent in a case of,
+        1. create a portfolio for given tickers
+
 I can call this tool and consider its output before generating a response.
 """
