f517856799069db5e01c75d37f6917b7dfffe9d3
Sandev <sandev.kuruppu@h2o.ai>
2025-02-07 01:59:11 +0530

    changes for new

--- src/ips_agent/ips_agent.py
+++ src/ips_agent/ips_agent.py
@@ -11,6 +11,30 @@ class IPSAgent:
         self.client = client
 
     def create(self):
+    # define a function with a docstring
+        def roll_dice() -> str:
+            """
+            Simulate the roll of a 20-sided die (d20).
+
+            This function generates a random integer between 1 and 20, inclusive,
+            which represents the outcome of a single roll of a d20.
+
+            Returns:
+                str: The result of the die roll.
+            """
+            import random
+
+            dice_role_outcome = random.randint(1, 20)
+            output_string = f"You rolled a {dice_role_outcome}"
+            return output_string
+
+
+        # create the tool
+        tool = self.client.tools.create_from_function(
+            func=roll_dice
+        )
+        print(f"Created tool {tool.name}")
+
         new_agent = self.client.agents.create(
             name=self.name,
             memory_blocks=[
@@ -21,6 +45,7 @@ class IPSAgent:
             ],
             model="openai/gpt-4o-mini",
             embedding="openai/text-embedding-ada-002",
+            tool_ids=[tool.id]
         )
 
         logger.info(f"Created {self.name} with ID: {new_agent.id}")
